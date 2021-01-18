4f19d8aeced2660e9f6d871da72a31bae40852b1
Jaco Greeff <jacogr@gmail.com>
2021-01-18 11:27:51 +0100

    Initial, not-yet-working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 import { AppComponent } from './app.component';
 
@@ -14,3 +15,14 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+async function initApi () {
+  const provider = new WsProvider('wss://kusama-rpc.polkadot.io');
+  const api = await ApiPromise.create({ provider });
+
+  api.rpc.chain.subscribeNewHeads((header) => {
+    console.log(header.number.toHuman());
+  });
+}
+
+initApi().catch(console.error);
